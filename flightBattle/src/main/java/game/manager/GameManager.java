f0d7580559d5bdc07d93f53301d528d15ec4834f
jeonggyuni86-cloud <jeonggyuni86@gmail.com>
2026-06-11 11:29:48 +0900

    flightGame

--- flightBattle/src/main/java/game/manager/GameManager.java
+++ flightBattle/src/main/java/game/manager/GameManager.java
@@ -5,14 +5,23 @@ import game.object.Enemy;
 import game.object.Player;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameManager {
-    private Player player;
-    private List<Enemy> enemies;
-    private List<Bullet> bullets;
-    private SpawnManager spawnManager;
-    private CollisionManager collisionManager;
+    private final Player player;
+    private final List<Enemy> enemies;
+    private final List<Bullet> bullets;
+    private final SpawnManager spawnManager;
+    private final CollisionManager collisionManager;
+
+    public GameManager(Player player, SpawnManager spawnManager, CollisionManager collisionManager) {
+        this.player = player;
+        this.enemies = new ArrayList<>();
+        this.bullets = new ArrayList<>();
+        this.spawnManager = spawnManager;
+        this.collisionManager = collisionManager;
+    }
 
 
     public void update() {
