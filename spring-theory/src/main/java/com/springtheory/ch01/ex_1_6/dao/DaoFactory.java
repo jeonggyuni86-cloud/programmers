c4e101a6b8ef9b8a2189c96e6bfc9b345968c3a1
jeonggyuni86-cloud <jeonggyuni86@gmail.com>
2026-06-23 14:06:23 +0900

    spring-theory

--- spring-theory/src/main/java/com/springtheory/ch01/ex_1_6/dao/DaoFactory.java
+++ spring-theory/src/main/java/com/springtheory/ch01/ex_1_6/dao/DaoFactory.java
@@ -4,16 +4,12 @@ package com.springtheory.ch01.ex_1_6.dao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// DaoFactory를 스프링 빈 팩토리가 사용할 수 있는 설정 정보로 리팩토링
-// Configuration 대신 Component 써도 되지만 유지 보수상 Configuration으로 사용
-// 기능상 차이가 없지만 유지보수를 위해 구분지어 놓은 것
-
-@Configuration // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정 정보라는 표시
+@Configuration
 public class DaoFactory {
 
-    @Bean // 오브젝트 생성을 담당하는 IoC용 메서드
+    @Bean
     public UserDAO userDAO() {
-        return new UserDAO(getConnectionMaker());
+        return UserDAO.getInstance(getConnectionMaker());
     }
 
     @Bean
