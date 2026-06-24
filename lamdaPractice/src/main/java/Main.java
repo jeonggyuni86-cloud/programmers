7a0c24acd538ea4abdd3bded6811b1e562aa3d56
jeonggyuni86-cloud <jeonggyuni86@gmail.com>
2026-06-24 09:15:54 +0900

    lambda

---
+++ lamdaPractice/src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    static void main(String[] args) {
+        Operation add = (a, b) -> a + b;
+        Operation sub = (a, b) -> a - b;
+
+        Add add1 = new Add();
+
+        System.out.println("lambda " + add.apply(10, 20));
+        System.out.println("lambda " + sub.apply(10, 20));
+
+        System.out.println("class " + add1.apply(10, 20));
+
+
+        String str = "lambda " + add.apply(10, 20);
+        Printer printer = System.out::println;
+        printer.print(str);
+
+    }
+}
---
+++ lamdaPractice/src/main/java/Operation.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Operation {
+    int apply(int a, int b);
+}
