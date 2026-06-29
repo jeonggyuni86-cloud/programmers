c37ba1f110e490f6dbb8b141964d61b356ef849d
jeonggyuni86-cloud <jeonggyuni86@gmail.com>
2026-06-29 10:28:57 +0900

    ch_5 _ 추상화

---
+++ spring-theory/src/main/java/com/springtheory/ch05/ex_5_1/dao/UserDAO.java
@@ -0,0 +1,44 @@
+package com.springtheory.ch05.ex_5_1.dao;
+
+import com.springtheory.ch05.ex_5_1.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class UserDAO {
+    private final JdbcContext jdbcContext;
+
+    public UserDAO(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
+    }
+
+    public void add(User user) throws  SQLException {
+        StatementStrategy strategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                var pStmt = connection.prepareStatement("INSERT INTO users (id, name, password) VALUES (?, ?, ?)");
+
+                pStmt.setString(1, user.getId());
+                pStmt.setString(2, user.getName());
+                pStmt.setString(3, user.getPassword());
+
+                return pStmt;
+            }
+        };
+        jdbcContext.workWithStatementStrategy(strategy);
+    }
+
+    // remove only test
+    public void deleteAll() throws ClassNotFoundException, SQLException {
+        var strategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                return connection.prepareStatement("DELETE FROM users");
+            }
+        };
+        jdbcContext.workWithStatementStrategy(strategy);
+    }
+
+}
