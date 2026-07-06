8f4ca994e35b2e39678cfd8eda27e012cd7cc8df
jeonggyuni86-cloud <jeonggyuni86@gmail.com>
2026-07-06 09:15:34 +0900

    basic_board

---
+++ weather_feign/src/test/java/com/weather_feign/WeatherFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather_feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
