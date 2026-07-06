3386397f30b783add80df8a0eaee72b474e6b02b
jeonggyuni86-cloud <jeonggyuni86@gmail.com>
2026-07-06 17:30:40 +0900

    회원가입 실습

--- springboot/board_practice/src/main/java/com/board_practice/controller/MemberApiController.java
+++ springboot/board_practice/src/main/java/com/board_practice/controller/MemberApiController.java
@@ -1,4 +1,44 @@
 package com.board_practice.controller;
 
+import com.board_practice.dto.LoginRequestDto;
+import com.board_practice.dto.LoginResponseDto;
+import com.board_practice.dto.MemberJoinRequestDto;
+import com.board_practice.dto.MemberJoinResponseDto;
+import com.board_practice.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
 public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public MemberJoinResponseDto join(
+            @RequestBody MemberJoinRequestDto memberJoinRequestDto
+    ) {
+        memberService.join(memberJoinRequestDto);
+        return new MemberJoinResponseDto("/members/login");
+    }
+
+
+    @PostMapping("/login")
+    public LoginResponseDto login(
+            @RequestBody LoginRequestDto loginRequestDto,
+            HttpSession session
+    ) {
+        return memberService.login(loginRequestDto)
+                .map(
+                        member -> {
+                            session.setAttribute("userId", member.getUserId());
+                            session.setAttribute("userName", member.getUserName());
+                            return LoginResponseDto.success("로그인 성공");
+                        }
+                ).orElseGet(() -> LoginResponseDto.fail("로그인 실패"));
+    }
 }
