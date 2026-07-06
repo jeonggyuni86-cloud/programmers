8cf2d5eab01f4348d8b3c1b6add2f658285dc5da
jeonggyuni86-cloud <jeonggyuni86@gmail.com>
2026-07-06 10:32:12 +0900

    basic-board

--- springboot/board/basic-board/src/main/java/com/basicboard/BasicBoardApplication.java
+++ springboot/board/basic-board/src/main/java/com/basicboard/BasicBoardApplication.java
@@ -8,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // controller의 매핑을 아래 규칙에 맞게 설계하면 RESTful 하다고 한다.
 
 // * 핵심원칙
-// - 자원 중심 URI : URI는 명사(자원)로, 동사를 쓰지 않는다.
+// - 자원 중심 URI : URI는 명사(자원)로, 동사를 쓰지 않는다. (ex. /boards (O), /getBoard (X))
+// - HTTP 메서드로 행위 구분
+// - GET : 조회 (예: GET /boards (복수형), GET /boards/1(id) (단수))
+// - POST : 생성 (예: POST /boards)
+// - PUT : 전체 수정 (예: PUT /boards/1 (1번내용전체 수정))
+// - PATCH : 부분 수정 (예: PATCH /boards/1 (1번 내용 부분수정))
+// - DELETE : 삭제 (예: DELETE /boards/1 (1번 전체삭제))
+
+// * 영속성(Persistence) - 관계 : ORM(개념) > JPA(표준)
 
 
 @SpringBootApplication
