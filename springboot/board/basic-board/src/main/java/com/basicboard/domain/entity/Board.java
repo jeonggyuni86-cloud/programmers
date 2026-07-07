0635ae1532d56c60d138ebe989b4c162c781dd6b
jeonggyuni86-cloud <jeonggyuni86@gmail.com>
2026-07-07 10:35:25 +0900

    Board Entity 추가

---
+++ springboot/board/basic-board/src/main/java/com/basicboard/domain/entity/Board.java
@@ -0,0 +1,38 @@
+package com.basicboard.domain.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.*;
+
+@Entity
+@Table(name = "board")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false, length = 50)
+    private String userId;
+
+    @Column
+    private String filePath;
+
+    @JsonFormat(pattern= "yyyy-MM-dd HH:mm")
+    private LocalDateTime created;
+
+}
