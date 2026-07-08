e5e3a715262dcf7457a2026ba52a47e9f07039c1
jeonggyuni86-cloud <jeonggyuni86@gmail.com>
2026-07-08 10:54:56 +0900

    파일 IO 추가

--- springboot/board/basic-board/src/main/java/com/basicboard/controller/BoardApiController.java
+++ springboot/board/basic-board/src/main/java/com/basicboard/controller/BoardApiController.java
@@ -6,7 +6,11 @@ import com.basicboard.dto.BoardListResponseDto;
 import com.basicboard.dto.BoardWriteRequestDto;
 import com.basicboard.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -53,6 +57,18 @@ public class BoardApiController {
                 .userId(board.getUserId())
                 .created(board.getCreated())
                 .build();
+    }
+
+    // ResponseEntity 는 HTTP 응답의 3가지를 직접 제어하게 해주는 래퍼다
+    // [상태 코드] + [헤더] + [본문]
+    // 그냥 Resource만 리턴하면 파일 내용은 내려가지만,
+    // Content-Disposition : attachment 헤더를 붙일 방법이 없다.
+    // -> 그러면 다운로드가 아니라 브라우저가 파일을 그냥 열어버리고, 저장 파일명도 지정할 수 없다
+
+    @GetMapping("/file/download/{fileName}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable("fileName") String fileName) {
+
+        return null;
     }
 
 
--- springboot/board/basic-board/src/main/java/com/basicboard/service/FileService.java
+++ springboot/board/basic-board/src/main/java/com/basicboard/service/FileService.java
@@ -1,11 +1,16 @@
 package com.basicboard.service;
 
+import com.basicboard.exception.BoardNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.MalformedInputException;
 import java.util.UUID;
 
 @Service
@@ -30,6 +35,26 @@ public class FileService {
             file.transferTo(dest);
             return storedFileName;
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Resource downloadFile(String fileName) {
+        try {
+            File baseUploadDir = new File(uploadDir).getAbsoluteFile();
+            File file = new File(baseUploadDir, fileName);
+
+            // 파일을 가리키는 Resource 생성 (실제로 읽어 들이는게 아니라 '위치만' 잡아 둔 상태)
+
+            Resource resource = new UrlResource(file.toURI());
+            if( !resource.exists() || !resource.isReadable() ) {
+                throw new IOException("파일을 읽어오는데 실패 했습니다. : " + fileName);
+            }
+            return resource;
+
+        } catch (MalformedInputException e) {
+            throw new IllegalStateException("파일 경로가 잘못 되었습니다.");
+        } catch(IOException e) {
             throw new RuntimeException(e);
         }
     }
