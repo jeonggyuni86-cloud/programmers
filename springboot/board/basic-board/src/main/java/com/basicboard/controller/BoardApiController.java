38b4b1b196a49252c93e15b6f04d31b1f43263a4
jeonggyuni86-cloud <jeonggyuni86@gmail.com>
2026-07-08 11:22:18 +0900

    파일 다운로드 추가

--- springboot/board/basic-board/src/main/java/com/basicboard/controller/BoardApiController.java
+++ springboot/board/basic-board/src/main/java/com/basicboard/controller/BoardApiController.java
@@ -8,6 +8,8 @@ import com.basicboard.service.BoardService;
 import com.basicboard.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -79,7 +81,22 @@ public class BoardApiController {
         //   - URLEncoder 는 공백을 '+' 로 바꾸는데, 파일명에선 '+' 가 그대로 보이면 곤란하므로 %20 으로 치환한다
         String encodedFileName = URLEncoder.encode(resource.getFilename(), StandardCharsets.UTF_8)
                 .replaceAll("\\+", "%20");
-        return null;
+
+        // contentType(MediaType.APPLICATION_OCTET_STREAM) -> 힌트
+        // 무슨 파일인지 특정하지 않은 순수 바이너리 라는 뜻.
+        // 브라우저가 열 방법을 몰라 저장 쪽으로 기울게 하는 '힌트' 일 뿐, 다운로드 확정하지 못한다.
+        // (확장자 등에 따라 브라우저가 그냥 열어버릴 수도 있다.)
+        // * header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename*=UTF-8''" + encodedFileName)
+        // attachment는 인라인으로 열지말고 무조건 첨부(다운로드)하라는 확실한 지시
+        // filename* 는 인코딩을 명시하는 최신 문법으로, "저장될 기본 파일명"을 정한다.
+        // 이게 없으면 URL끝의 UUID 붙은 이름으로 저장돼 버린다. 그래서 원본 이름으로 저장하게 되는 것
+        // UTF-8 뒤에 '' 안에는 언어 지정핼 수 있으나, 이경우 생략 (ex UTF-8'ko')
+        //
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename*=UTF-8''" + encodedFileName)
+                .body(resource);
     }
 
 
