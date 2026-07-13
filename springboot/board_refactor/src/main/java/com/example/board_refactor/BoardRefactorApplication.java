b714c0d2ff94bf0eaa5bb70f067a5771e9ea36ee
ijeong-gyun <jeonggyuni86@gmail.com>
2026-07-13 09:04:53 +0900

    board_refactor

---
+++ springboot/board_refactor/src/main/java/com/example/board_refactor/BoardRefactorApplication.java
@@ -0,0 +1,13 @@
+package com.example.board_refactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardRefactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardRefactorApplication.class, args);
+    }
+
+}
