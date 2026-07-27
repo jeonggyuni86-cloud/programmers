2f0db0dfc80a7e705a24a6466f4ae17de38f86d3
ijeong-gyun <jeonggyuni86@gmail.com>
2026-07-27 09:05:17 +0900

    security-set-up

---
+++ springboot/htttp-basic/src/main/java/com/example/htttpbasic/HtttpBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.htttpbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtttpBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtttpBasicApplication.class, args);
+    }
+
+}
---
+++ springboot/htttp-basic/src/test/java/com/example/htttpbasic/HtttpBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htttpbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtttpBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
