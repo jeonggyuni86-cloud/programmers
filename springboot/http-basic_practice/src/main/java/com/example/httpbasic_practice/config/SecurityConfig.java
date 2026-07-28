53cc5996bc7c2cb9f77e376c4fc65f0bdff9d853
ijeong-gyun <jeonggyuni86@gmail.com>
2026-07-28 17:32:44 +0900

    http-basic-practice

--- springboot/http-basic_practice/src/main/java/com/example/httpbasic_practice/config/SecurityConfig.java
+++ springboot/http-basic_practice/src/main/java/com/example/httpbasic_practice/config/SecurityConfig.java
@@ -1,4 +1,43 @@
 package com.example.httpbasic_practice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var manager = new InMemoryUserDetailsManager();
+        var user = User
+                .withUsername("user")
+                .password("12345")
+                .authorities("USER")
+                .build();
+        manager.createUser(user);
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
--- springboot/http-basic_practice/src/main/java/com/example/httpbasic_practice/controller/HelloApiController.java
+++ springboot/http-basic_practice/src/main/java/com/example/httpbasic_practice/controller/HelloApiController.java
@@ -1,4 +1,13 @@
 package com.example.httpbasic_practice.controller;
 
-public class HelloController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloApiController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
 }
