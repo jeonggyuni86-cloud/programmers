a40d9b292d956d31f4156e2670b77d2311471ff1
ijeong-gyun <jeonggyuni86@gmail.com>
2026-07-29 11:29:31 +0900

    token validate

--- springboot/token/src/main/java/com/example/token/config/jwt/TokenProvider.java
+++ springboot/token/src/main/java/com/example/token/config/jwt/TokenProvider.java
@@ -1,8 +1,19 @@
 package com.example.token.config.jwt;
 
+import com.example.token.domain.entitiy.User;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.Duration;
+import java.util.Base64;
+import java.util.Date;
 
 // * 토큰 생성/검증/해석을 전담하는 컴포넌트
 // - generateToken : User 정보를 클레임에 담아 서명된 jwt 문자열 생성
@@ -18,4 +29,54 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class TokenProvider {
+
+    private static final String CLAIM_ID = "id";
+    private static final String CLAIM_NAME = "name";
+    private static final String CLAIM_ROLE = "role";
+
+    private final JwtProperties jwtProperties;
+
+    private SecretKey secretKey;
+    private JwtParser jwtParser;
+
+    @PostConstruct
+    public void init() {
+        this.secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtProperties.getSecretKey()));
+        this.jwtParser = Jwts.parser().verifyWith(secretKey).build();
+    }
+
+    public String generateToken(User user, Duration expiredAt) {
+        Date now = new Date();
+        return makeToken(
+                user,
+                new Date(now.getTime() + expiredAt.toMillis())
+        );
+    }
+
+    private String makeToken(User user, Date expire) {
+        return Jwts.builder()
+                .header().type("JWT").and()
+                .issuer(jwtProperties.getIssuer())
+                .issuedAt(new Date())
+                .expiration(expire)
+                .subject(user.getUserId())
+                .claim(CLAIM_ID, user.getId())
+                .claim(CLAIM_NAME, user.getName())
+                .claim(CLAIM_ROLE, user.getRole())
+                .signWith(secretKey, Jwts.SIG.HS512)
+                .compact();
+    }
+
+    public TokenStatus validateToken(String token) {
+        try {
+            jwtParser.parseSignedClaims(token);
+            log.debug("Token is Valid");
+        } catch(ExpiredJwtException e) {
+            log.warn("Token is Expired");
+            return TokenStatus.EXPIRED;
+        } catch(Exception e) {
+            log.warn("Token is Invalid");
+            return TokenStatus.INVALID;
+        }
+    }
 }
