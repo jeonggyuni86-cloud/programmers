bf5228c4f81baffd388574793e82c1530d4dd707
ijeong-gyun <jeonggyuni86@gmail.com>
2026-07-30 10:35:54 +0900

    token api

--- springboot/token/src/main/java/com/example/token/controller/TokenApiController.java
+++ springboot/token/src/main/java/com/example/token/controller/TokenApiController.java
@@ -1,10 +1,15 @@
 package com.example.token.controller;
 
 import com.example.token.config.jwt.JwtProperties;
+import com.example.token.dto.ErrorResponseDto;
+import com.example.token.dto.RefreshTokenResponse;
 import com.example.token.service.TokenService;
+import com.example.token.util.CookieUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +23,30 @@ public class TokenApiController {
     private final JwtProperties jwtProperties;
 
     @PostMapping("/refresh")
-    public void refreshToken(
+    public ResponseEntity<?> refreshToken(
             HttpServletRequest request,
             HttpServletResponse response
     ) {
+        RefreshTokenResponse refreshTokenResponse = tokenService.refreshToken(request.getCookies());
+
+        if(refreshTokenResponse.isValidated()) {
+
+            CookieUtil.addCookie(
+                    response,
+                    CookieUtil.REFRESH_TOKEN_COOKIE,
+                    refreshTokenResponse.getRefreshToken(),
+                    (int)jwtProperties.getRefreshTokenValidity().toSeconds()
+            );
+            return ResponseEntity.ok(refreshTokenResponse);
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponseDto(
+                        HttpStatus.UNAUTHORIZED.value(),
+                        "리프레시 토큰이 만료되었습니다"
+                        )
+                );
 
     }
 }
