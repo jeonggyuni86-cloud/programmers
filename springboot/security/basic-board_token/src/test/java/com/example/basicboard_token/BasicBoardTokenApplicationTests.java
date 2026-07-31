3e5288992e485347a44a427472a71f84778d4039
ijeong-gyun <jeonggyuni86@gmail.com>
2026-07-31 09:32:24 +0900

    b-board-token-setup

---
+++ springboot/security/basic-board_token/src/test/java/com/example/basicboard_token/BasicBoardTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicboard_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBoardTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
