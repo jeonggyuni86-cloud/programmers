131ddaa3113273e5eaeb05dad3875aaca6aefce7
ijeong-gyun <jeonggyuni86@gmail.com>
2026-08-04 14:09:04 +0900

    init

---
+++ springboot/security/oauth2_basic_board/src/main/java/com/example/oauth2_basic_board/Oauth2BasicBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_basic_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2BasicBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2BasicBoardApplication.class, args);
+    }
+
+}
---
+++ springboot/security/oauth2_basic_board/src/test/java/com/example/oauth2_basic_board/Oauth2BasicBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_basic_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2BasicBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
